691d3fe1ab9640952d28dd7bdae305e991b7e778
TheBoringDude <iamcoderx@gmail.com>
2021-01-01 00:47:15 +0800

    Integrate APIs for item searching

--- frontend/src/components/Modal.js
+++ frontend/src/components/Modal.js
@@ -1,11 +1,129 @@
 import React, { useState, useRef } from 'react'
-import { Modal, Button, Form } from 'react-bootstrap'
+import { Modal, Button, Form, Row } from 'react-bootstrap'
+import SearchResult from './items/Results'
+import axios from 'axios'
 
 export default function CollectionModal(props) {
   const collection = props.collection
+  const sources = {
+    movies: 'TMDB (TheMoveDatabase.org)',
+    seris: 'TMDB (TheMoveDatabase.org)',
+    anime: 'Jikan API (MyAnimeList.net)',
+    manga: 'Jikan API (MyAnimeList.net)',
+    asian_drama: 'Kuryana API (MyDramaList.com)',
+    books: 'OpenLibrary.org',
+  }
 
   const [addModal, setAddModal] = useState(false)
+  const [searching, setSearching] = useState(false)
+  const [searchResults, setSearchResults] = useState({})
   const searchInput = useRef(null)
+
+  // handle cover images
+  // it returns false if there
+  // are no cover images present
+  // from the response
+  const handleCoverImage = (type, res) => {
+    if (type === 'movies' || type === 'series') {
+      if (res.poster_path === undefined || res.backdrop_path === undefined) {
+        return false
+      }
+    } else if (type === 'anime' || type === 'manga') {
+      if (res.image_url === undefined) {
+        return false
+      }
+    } else if (type === 'asian_drama') {
+      if (res.thumb === undefined) {
+        return false
+      }
+    } else if (type === 'books') {
+      if (res.cover_i === undefined) {
+        return false
+      }
+    }
+
+    return true
+  }
+
+  // main handler for searching items
+  const handleSearch = (e) => {
+    // prevent auto-form submit and reload
+    e.preventDefault()
+
+    setSearching(true)
+    if (collection.type === 'movies') {
+      return GetMoviesSeries('movie')
+    } else if (collection.type === 'series') {
+      return GetMoviesSeries('tv')
+    } else if (collection.type === 'anime') {
+      return GetAnimeManga('anime')
+    } else if (collection.type === 'manga') {
+      return GetAnimeManga('manga')
+    } else if (collection.type === 'asian_drama') {
+      return GetKDrama()
+    } else if (collection.type === 'books') {
+      return GetBooks()
+    }
+  }
+
+  // movies & series handlers || both have similar API structures
+  // REACT_APP_TMDB_KEY => set this TMDB api with your own
+  const GetMoviesSeries = (type) => {
+    axios
+      .get(
+        `https://api.themoviedb.org/3/search/${type}?api_key=${process.env.REACT_APP_TMDB_KEY}&language=en-US&query=${searchInput.current.value}&page=1&include_adult=false`,
+      )
+      .then((res) => {
+        const data = res.data
+
+        setSearchResults(data.results)
+        setSearching(false)
+      })
+      .catch((err) => console.error(err))
+  }
+
+  // anime & manga handlers || both have similar API structures
+  const GetAnimeManga = (type) => {
+    axios
+      .get(
+        `https://api.jikan.moe/v3/search/${type}?q=${searchInput.current.value}&page=1`,
+      )
+      .then((res) => {
+        const data = res.data
+
+        setSearchResults(data.results)
+        setSearching(false)
+      })
+      .catch((err) => console.error(err))
+  }
+
+  // kdrama handler
+  const GetKDrama = () => {
+    axios
+      .get(`https://kuryana.vercel.app/search/q/${searchInput.current.value}`)
+      .then((res) => {
+        const data = res.data
+
+        setSearchResults(data.results)
+        setSearching(false)
+      })
+      .catch((err) => console.error(err))
+  }
+
+  // books handler
+  const GetBooks = () => {
+    axios
+      .get(
+        `http://openlibrary.org/search.json?title=${searchInput.current.value}&page=1`,
+      )
+      .then((res) => {
+        const data = res.data
+
+        setSearchResults(data.results)
+        setSearching(false)
+      })
+      .catch((err) => console.error(err))
+  }
 
   return (
     <>
@@ -38,13 +156,18 @@ export default function CollectionModal(props) {
         keyboard={false}
       >
         <Modal.Header closeButton>
-          <Modal.Title>Find and Add New Item</Modal.Title>
+          <Modal.Title>
+            find and add new{' '}
+            <span className="font-weight-bold">{collection.type}</span>
+          </Modal.Title>
         </Modal.Header>
         <Modal.Body>
           <div className="w-75 mx-auto">
-            <Form>
+            <Form onSubmit={handleSearch}>
               <Form.Group controlId="collection-name">
-                <Form.Label>Search the title of the item to add</Form.Label>
+                <Form.Label>
+                  Search the title of the {collection.type} to add
+                </Form.Label>
                 <div className="d-flex align-items-center">
                   <Form.Control
                     ref={searchInput}
@@ -61,6 +184,38 @@ export default function CollectionModal(props) {
               </Form.Group>
             </Form>
           </div>
+
+          {/* searching for item message */}
+          {searching ? (
+            <div>
+              <p>Finding for item '{searchInput.current.value}'...</p>
+            </div>
+          ) : null}
+
+          {/* search output */}
+          {searchResults.length > 0 ? (
+            <div className="mt-2">
+              <h6>
+                Results provided by: <span>{sources[collection.type]}</span>
+              </h6>
+
+              <hr />
+
+              <div>
+                <Row md={3}>
+                  {searchResults.map((res) =>
+                    handleCoverImage(collection.type, res) ? (
+                      <SearchResult
+                        key={searchResults.indexOf(res)}
+                        result={res}
+                        type={collection.type}
+                      />
+                    ) : null,
+                  )}
+                </Row>
+              </div>
+            </div>
+          ) : null}
         </Modal.Body>
       </Modal>
     </>
---
+++ frontend/src/components/items/Results.js
@@ -0,0 +1,91 @@
+import React from 'react'
+import { Col, Image, Button } from 'react-bootstrap'
+
+export default function SearchResult(props) {
+  const result = props.result
+
+  var resTitle = ''
+  var resCover = ''
+
+  if (props.type === 'movies') {
+    // set result title
+    if (result.release_date !== undefined) {
+      resTitle = result.title + ' (' + result.release_date.split('-')[0] + ')'
+    } else {
+      resTitle = result.title
+    }
+
+    // set result cover
+    if (result.poster_path !== undefined) {
+      resCover =
+        'https://image.tmdb.org/t/p/w600_and_h900_bestv2/' + result.poster_path
+    } else {
+      resCover =
+        'https://image.tmdb.org/t/p/w600_and_h900_bestv2/' + result.poster_path
+    }
+  }
+  // series
+  else if (props.type === 'series') {
+    // set result title
+    if (result.first_air_date !== undefined) {
+      resTitle = result.name + ' (' + result.first_air_date.split('-')[0] + ')'
+    } else {
+      resTitle = result.name
+    }
+
+    // set result cover
+    if (result.poster_path !== undefined) {
+      resCover =
+        'https://image.tmdb.org/t/p/w600_and_h900_bestv2/' + result.poster_path
+    } else {
+      resCover =
+        'https://image.tmdb.org/t/p/w600_and_h900_bestv2/' + result.poster_path
+    }
+  }
+  // anime || manga
+  else if (props.type === 'anime' || props.type === 'manga') {
+    // set result title
+    if (result.start_date !== undefined) {
+      resTitle = result.title + ' (' + result.start_date.split('-')[0] + ')'
+    } else {
+      resTitle = result.title
+    }
+
+    // set result cover
+    resCover = result.image_url
+  }
+  // asian dramas
+  else if (props.type === 'asian_drama') {
+    // set result title
+    if (result.year !== undefined) {
+      resTitle = result.title + ' (' + result.year + ')'
+    } else {
+      resTitle = result.title
+    }
+
+    // set result cover
+    resCover = result.thumb.replace('s.jpg', 'c.jpg')
+  }
+  // books
+  else if (props.type === 'books') {
+    // set result title
+    resTitle = result.title
+
+    // set result cover
+    resCover = `https://covers.openlibrary.org/b/id/${result.cover_i}-L.jpg`
+  }
+
+  return (
+    <Col>
+      <div className="mb-4 bg-light">
+        <div>
+          <Image fluid src={resCover} />
+        </div>
+        <div className="text-center mt-2 p-3">
+          <h5 className="font-weight-light">{resTitle}</h5>
+          <Button variant="primary">add item</Button>
+        </div>
+      </div>
+    </Col>
+  )
+}
